d01d843cfd57e84e28d07ace5648746303f6634a
vadim210607 <vadim210607@gmail.com>
2023-06-09 22:35:38 +0300

    some change before deploy

--- static/css/general.js
+++ static/css/general.js
@@ -12,8 +12,8 @@ $('#detail .main-img-slider').slick({
   speed: 300,
   lazyLoad: 'ondemand',
   asNavFor: '.thumb-nav',
-  prevArrow: '<div class="slick-prev"><img src="/static/icon/left.svg"></div>',
-  nextArrow: '<div class="slick-next"><img src="/static/icon/circle-right.svg"></div>' //
+  prevArrow: '<div class="slick-prev slick-prev-im "><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></div>',
+  nextArrow: '<div class="slick-next slick-next-im"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></div>',
 });
 
 // Thumbnail/alternates
@@ -28,8 +28,8 @@ $('.thumb-nav').slick({
   draggable: true,
   speed:200,
   focusOnSelect: true,
-  prevArrow: '<div class="slick-prev"><img src="/static/icon/left.svg"></div>',
-  nextArrow: '<div class="slick-next"><img src="/static/icon/right.svg"></div>',
+  prevArrow: '<div class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></div>',
+  nextArrow: '<div class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></div>',
 // SHEMA ROBOCHA
 //  responsive: [
 //    {
